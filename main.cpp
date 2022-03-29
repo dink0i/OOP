16c8f89caa7843d0d86cf1fe868be69e127276ba
Koi <nguyenkhoi.hcmus@gmail.com>
2022-03-29 14:46:56 +0700

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,68 @@
 #include<iostream>
 using namespace std;
 
+class sach
+{
+    private:
+    string ma_sach;
+    string ten_sach;
+    string the_loai;
+    string tac_gia;
+    int don_gia;
+    public:
+    sach()
+    {
+        ma_sach="noID";
+        ten_sach="unName";
+        the_loai="noType";
+        tac_gia="noAuthor";
+        don_gia=0;
+    }
+    void nhap_ten_sach(string ten_sach)
+    {
+        this->ten_sach=ten_sach;
+    }
+    string ten_sach()
+    {
+        return ten_sach;
+    }
+    void nhap_ma_sach(string ma_sach)
+    {
+        this->ma_sach=ma_sach;
+    }
+    string ma_sach()
+    {
+        return ma_sach;
+    }
+    void nhap_tac_gia(string tac_gia)
+    {
+        this->tac_gia=tac_gia;
+    }
+    string tac_gia()
+    {
+        return tac_gia;
+    }
+    void nhap_don_gia(int don_gia)
+    {
+        this->don_gia=don_gia;
+    }
+    int don_gia()
+    {
+        return don_gia;
+    }
+};
 
+class kho_sach:public sach
+{
+    private:
+    int so_luong;
+    public:
+    kho_sach() :sach()
+    {
+        so_luong=0;
+    }
+
+};
 
 int main()
 {
