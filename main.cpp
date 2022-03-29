9faae74a4a4d1ab8adf5740b9cb7807b33692a13
Koi <nguyenkhoi.hcmus@gmail.com>
2022-03-29 15:22:16 +0700

    new

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class sach
@@ -9,6 +10,7 @@ class sach
     string the_loai;
     string tac_gia;
     int don_gia;
+    int so_luong;
     public:
     sach()
     {
@@ -17,12 +19,13 @@ class sach
         the_loai="noType";
         tac_gia="noAuthor";
         don_gia=0;
+        so_luong=0;
     }
-    void nhap_ten_sach(string ten_sach)
+    void nhap_ten_sach()
     {
-        this->ten_sach=ten_sach;
+        cin>>ten_sach;
     }
-    string ten_sach()
+    string lay_ten_sach()
     {
         return ten_sach;
     }
@@ -30,7 +33,7 @@ class sach
     {
         this->ma_sach=ma_sach;
     }
-    string ma_sach()
+    string lay_ma_sach()
     {
         return ma_sach;
     }
@@ -38,7 +41,7 @@ class sach
     {
         this->tac_gia=tac_gia;
     }
-    string tac_gia()
+    string lay_tac_gia()
     {
         return tac_gia;
     }
@@ -46,26 +49,57 @@ class sach
     {
         this->don_gia=don_gia;
     }
-    int don_gia()
+    int lay_don_gia()
     {
         return don_gia;
+    }
+    
+    void display()
+    {
+        cout<<ma_sach<<endl;
+        cout<<ten_sach<<endl;
+        cout<<the_loai<<endl;
+        cout<<tac_gia<<endl;
+        cout<<don_gia<<endl;
     }
 };
 
 class kho_sach:public sach
 {
     private:
+    vector<sach> a;
     int so_luong;
     public:
     kho_sach() :sach()
     {
-        so_luong=0;
+       so_luong=0; 
+    }
+    void nhap_sach()
+    {
+        int n;
+        cout<<"Nhap so luong sach:"; cin>>n;
+        so_luong += n;
+        for(int i=so_luong - n; i<so_luong; i++ )
+        {
+            cout<<"--------------";
+            int s=1;
+            cout<<s<<endl;
+            cout<<"nhap ten sach:";
+            a[i].nhap_ten_sach();   
+        }
+    }
+    void display()
+    {
+        sach::display();
+        cout<<so_luong<<endl;
     }
 
 };
 
 int main()
 {
-    cout<<"Hello Koi!";
+    kho_sach a;
+    a.nhap_sach();
+    a.display();
     return 0;
 }
