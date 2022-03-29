315a46765f743eec45d1d2ea8bedf3d767c5d2f6
unknown <thathcmus@gmail.com>
2022-03-29 14:31:26 +0700

    ver 3.0

--- NhaSach.cpp
+++ NhaSach.cpp
@@ -4,75 +4,117 @@
 
 using namespace std;
 
-class NhapSach{
+class EnterBook{
 private:
-	string ten_sach;
-	string tac_gia;
-	string the_loai;
-	int so_luong;
+	string book_name;
+	string book_code;
+	string book_author;
+	string book_type;
+	int book_amount;
+	int book_cost;
 public:
 	
-	NhapSach(){
+	EnterBook(){
 	}
-	NhapCacSach(){
+	EnterBooks(){
 		cout << "\n>  Nhap ten sach: ";
 		fflush(stdin);
-		getline(cin, this->tensach);
+		getline(cin, this->book_name);	//nhap
+		
+		cout << "\n>  Nhap ma sach: ";
+		fflush(stdin);
+		getline(cin, this->book_code);	//nhap
 			
 		cout << ">  Nhap tac gia: ";
 		fflush(stdin);
-		getline(cin, this->tacgia);	
+		getline(cin, this->book_author);
 		
 		cout << ">  Nhap the loai: ";
 		fflush(stdin);
-		getline(cin, this->theloai);	
+		getline(cin, this->book_type);	
 			
 		cout << ">  Nhap so luong: ";
 		fflush(stdin);
-		cin >> this->soluong;
+		cin >> this->book_amount;
+		
+		cout << ">  Nhap gia sach: ";
+		fflush(stdin);
+		cin >> this->book_cost;
 	}
-	InCacSachVuaNhap(){
-
-		cout << this->tensach << "\t\t\t" << this->tacgia << "\t\t\t" << this->theloai << "\t\t\t" << this->soluong << "\n";
+	PrintNewEnterBook(){
+		cout << this->book_name 	<< "\t\t\t" <<	this->book_code 	<< "\t\t\t" << 
+				this->book_author 	<< "\t\t\t" << 	this->book_type 	<< "\t\t\t"<<
+				this->book_amount 	<<"\t\t\t" 	<< 	this->book_amount 	<< "\n";
 		fstream FileInSachNhap;
 		FileInSachNhap.open("FileInSachNhap.txt", ios::out|ios::app);
-		FileInSachNhap << "\n" + tensach + "\t\t\t";
-		FileInSachNhap << tacgia + "\t\t\t";
-		FileInSachNhap << theloai + "\t\t\t";
-		FileInSachNhap << soluong;
+		FileInSachNhap << "\n" + book_name + "\t\t\t";
+		FileInSachNhap << book_code + "\t\t\t";
+		FileInSachNhap << book_author + "\t\t\t";
+		FileInSachNhap << book_type + "\t\t\t";
+		FileInSachNhap << book_amount + "\t\t\t";
+		FileInSachNhap << book_cost;
 	}
 	InTongCacSach(){
 		
 	}
 };
 
+
 int main(){
 	while(true){
 		cout << "=====================================================================================================\n";
 		cout << "======PHAN MEM QUAN LY NHA SACH======\n";
 		cout << "======************MENU*********======\n";
-		cout << "1. NHAP SACH VA IN DANH SACH VUA NHAP\n";
+		cout << "1. NHAP SACH MOI:\n";
 		
 		int key;
 		cout << ">> CHON: ";
 		cin >> key;
+		int num_book_name = 0;
 		switch(key){
 			case 1:
 				system("cls");
-				int sltensach = 0;
-				cout << "> Nhap so luong ten sach can nhap: ";
-				cin >> sltensach;
+				EnterBook a[num_book_name];
+				a[num_book_name].EnterBooks();
 				system("cls");
-				NhapSach a[sltensach];
-				for(int i = 0; i < sltensach; i++){
-					cout << ">> NHAP THONG TIN TEN SACH THU " << i + 1;
-					a[i].NhapCacSach();
-					system("cls");
-				}
 				cout << "===DANH SACH CAC SACH DA NHAP===\n";
 				cout << "Ten sach\t\t\tTac gia\t\t\tThe loai\t\t\tSo luong\n";
-				for(int i = 0; i < sltensach; i++) 
-					a[i].InCacSachVuaNhap();
+				a[num_book_name].PrintNewEnterBook();
+				num_book_name++;
+				break;
+		}
+	}
+	return 0;
+}
+/*
+int main(){
+	while(true){
+		cout << "=====================================================================================================\n";
+		cout << "======PHAN MEM QUAN LY NHA SACH======\n";
+		cout << "======************MENU*********======\n";
+		cout << "1. NHAP SACH MOI:\n";
+		
+		int key;
+		cout << ">> CHON: ";
+		cin >> key;
+		int num_book_name = 0;
+		switch(key){
+			case 1:
+				system("cls");
+//				int num_book_name = 0;
+//				cout << "> Nhap so luong ten sach can nhap: ";
+//				cin >> num_book_name;
+//				system("cls");
+				EnterBook a[num_book_name];
+//				for(int i = 0; i < num_book_name; i++){
+//				cout << ">> NHAP THONG TIN TEN SACH THU " << i + 1;
+				a[num_book_name].EnterBooks();
+				system("cls");
+//				}
+				cout << "===DANH SACH CAC SACH DA NHAP===\n";
+				cout << "Ten sach\t\t\tTac gia\t\t\tThe loai\t\t\tSo luong\n";
+				for(int i = 0; i < num_book_name; i++) 
+					a[i].PrintNewEnterBook();
 				break;
 		}
 	}
@@ -80,3 +122,7 @@ int main(){
 //	cout << "===DANH SACH CAC SACH DA NHAP===\n";
 	
 }
+
+*/
+
+
